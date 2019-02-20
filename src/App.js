6c2de01afb604bde38f63b182d2024b8cf7411bf
Johns, Alwin <Alwin.Johns@sabre.com>
2019-02-20 15:58:29 -0600

    initial commit with jenkins and react

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+	axios.get('http://simple-pipeline-example-sg0302490-free-tier.apps.ocusw2.scs.dev.ascint.sabrecirrus.com/', {
+			'Access-Control-Allow-Origin': "*"
+		  })
+		  .then(response => {
+		    console.log(response.data);
+		    console.log(JSON.stringify(response.data));
+		  })
+		  .catch(error => {
+		    console.log(error);
+		  });
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
